985230a052533bf8dc0ec83684df1d768a4da5e9
dimitrmo <dmouratidis153@gmail.com>
2023-02-10 17:59:59 +0200

    Convert pascal case

--- src/pubsub.rs
+++ src/pubsub.rs
@@ -2,11 +2,13 @@ use rhiaqey_sdk::channel::ChannelList;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub enum RPCMessageType {
     AssignChannels(ChannelList)
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub struct RPCMessage {
     pub data: RPCMessageType,
 }
