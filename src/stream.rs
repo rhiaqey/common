acaf67d609b14d05b65d7f83d81912ab3d94cff1
dimitrmo <dmouratidis153@gmail.com>
2023-04-18 12:04:06 +0300

    Implement Eq trait

--- src/stream.rs
+++ src/stream.rs
@@ -10,7 +10,7 @@ pub enum StreamMessageDataType {
     PartialData = 1, // sent by the hub to the client with partial data (used for partial updates)
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct StreamMessage {
     // type of data we are sending to user
     #[serde(rename = "typ")]
