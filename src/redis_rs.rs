26147b5eefbaacc7635138974a9f7e8963000fa6
Dimitrios Mouratidis <76875895+dimitrmo@users.noreply.github.com>
2024-03-15 12:31:57 +0000

    Updates

--- src/redis_rs.rs
+++ src/redis_rs.rs
@@ -1,15 +1,15 @@
 use crate::{error::RhiaqeyError, redis::RedisSettings};
 
-pub fn connect(settings: RedisSettings) -> Result<redis::Client, RhiaqeyError> {
-    let password = settings.redis_password.unwrap();
+pub fn connect(settings: &RedisSettings) -> Result<redis::Client, RhiaqeyError> {
+    let password = settings.redis_password.as_ref().unwrap();
 
-    let connect_uri = match settings.redis_address {
+    let connect_uri = match settings.redis_address.as_ref() {
         None => format!(
             "redis+sentinel://:{}@{}/{}?sentinel_password={}",
-            password.clone(),
-            settings.redis_sentinel_addresses.unwrap(),
-            settings.redis_sentinel_master.unwrap(),
-            password.clone()
+            password,
+            settings.redis_sentinel_addresses.as_ref().unwrap(),
+            settings.redis_sentinel_master.as_ref().unwrap(),
+            password,
         ),
         Some(address) => format!(
             "redis://:{}@{}",
@@ -22,7 +22,7 @@ pub fn connect(settings: RedisSettings) -> Result<redis::Client, RhiaqeyError> {
     Ok(client)
 }
 
-pub fn connect_and_ping(settings: RedisSettings) -> Result<redis::Client, RhiaqeyError> {
+pub fn connect_and_ping(settings: &RedisSettings) -> Result<redis::Client, RhiaqeyError> {
     let client = connect(settings)?;
     let mut connection = client.get_connection()?;
     let result: String = redis::cmd("PING").query(&mut connection)?;
