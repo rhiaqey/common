e51fc44a20df736affc63efc8b6434bdc2fe68dd
dimitrmo <dmouratidis153@gmail.com>
2024-03-23 14:10:41 +0200

    Password

--- src/redis_rs.rs
+++ src/redis_rs.rs
@@ -1,6 +1,6 @@
-use crate::{error::RhiaqeyError, redis::RedisSettings};
+use crate::{redis::RedisSettings, RhiaqeyResult};
 
-pub fn connect(settings: &RedisSettings) -> Result<redis::Client, RhiaqeyError> {
+pub fn connect(settings: &RedisSettings) -> RhiaqeyResult<redis::Client> {
     let password = settings.redis_password.as_ref().unwrap();
 
     let connect_uri = match settings.redis_address.as_ref() {
@@ -12,7 +12,7 @@ pub fn connect(settings: &RedisSettings) -> Result<redis::Client, RhiaqeyError> 
             password,
         ),
         Some(address) => format!(
-            "redis://{}@{}",
+            "redis://:{}@{}",
             password,
             address
         ),
@@ -22,8 +22,9 @@ pub fn connect(settings: &RedisSettings) -> Result<redis::Client, RhiaqeyError> 
     Ok(client)
 }
 
-pub fn connect_and_ping(settings: &RedisSettings) -> Result<redis::Client, RhiaqeyError> {
+pub fn connect_and_ping(settings: &RedisSettings) -> RhiaqeyResult<redis::Client> {
     let client = connect(settings)?;
+
     let mut connection = client.get_connection()?;
     let result: String = redis::cmd("PING").query(&mut connection)?;
     if result != "PONG" {
