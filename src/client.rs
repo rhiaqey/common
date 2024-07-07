db96603fbb50dd164da435f1683d5cf3792fc2e7
dimitrmo <dmouratidis153@gmail.com>
2024-07-07 10:57:13 +0300

    Shorten field name

--- src/client.rs
+++ src/client.rs
@@ -11,18 +11,18 @@ pub enum ClientMessageDataType {
     Ping = 100,           // sent by the hub to keep the client connect alive
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct ClientMessageValueClientConnection {
     pub client_id: String,
     pub hub_id: String,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub struct ClientMessageValueClientChannelSubscription {
     pub channel: Channel,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 #[serde(untagged)]
 pub enum ClientMessageValue {
     ClientConnection(ClientMessageValueClientConnection),
@@ -33,31 +33,31 @@ pub enum ClientMessageValue {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ClientMessage {
-    #[serde(rename = "typ")]
+    #[serde(rename = "d", alias = "typ")]
     pub data_type: u8,
 
-    #[serde(rename = "chn", skip_serializing_if = "String::is_empty")]
+    #[serde(rename = "c", alias = "chn", skip_serializing_if = "String::is_empty")]
     pub channel: String,
 
-    #[serde(rename = "key", skip_serializing_if = "String::is_empty")]
+    #[serde(rename = "k", alias = "key", skip_serializing_if = "String::is_empty")]
     pub key: String,
 
-    #[serde(rename = "val")]
+    #[serde(rename = "v", alias = "val")]
     pub value: ClientMessageValue,
 
-    #[serde(rename = "tag", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "t", alias = "tag", skip_serializing_if = "Option::is_none")]
     pub tag: Option<String>,
 
     // Extra grouping
-    #[serde(rename = "cat", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "g", alias = "cat", skip_serializing_if = "Option::is_none")]
     pub category: Option<String>,
 
     // hub_id is actually hub id. useful for debugging
-    #[serde(rename = "hid", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "h", alias = "hid", skip_serializing_if = "Option::is_none")]
     pub hub_id: Option<String>,
 
     // gateway or producer id, useful for debugging
-    #[serde(rename = "pid", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "p", alias = "pid", skip_serializing_if = "Option::is_none")]
     pub publisher_id: Option<String>,
 }
 
@@ -88,5 +88,68 @@ impl From<&StreamMessage> for ClientMessage {
             hub_id: value.hub_id.clone(),
             publisher_id: value.publisher_id.clone(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::client::{ClientMessage, ClientMessageDataType, ClientMessageValue};
+    use rhiaqey_sdk_rs::message::MessageValue;
+
+    #[test]
+    fn can_serialize() {
+        let client_message = ClientMessage {
+            data_type: ClientMessageDataType::Data as u8,
+            channel: "channel_1".to_string(),
+            key: "key_1".to_string(),
+            value: ClientMessageValue::Data(MessageValue::Text(String::from("some text"))),
+            tag: None,
+            category: None,
+            hub_id: None,
+            publisher_id: None,
+        };
+
+        let serialized = serde_json::to_string(&client_message).unwrap_or_default();
+        assert!(!serialized.is_empty());
+        assert!(serialized.contains("\"k\""));
+        assert!(serialized.contains("\"key_1\""));
+    }
+
+    #[test]
+    fn can_deserialize() {
+        let serialized_message =
+            "{\"d\":10,\"c\":\"channel_1\",\"k\":\"key_1\",\"v\":\"some text\"}";
+        let client_message = serde_json::from_str::<ClientMessage>(serialized_message);
+        let data_type = ClientMessageDataType::Data as u8;
+        let value = ClientMessageValue::Data(MessageValue::Text(String::from("some text")));
+        assert!(client_message.is_ok());
+        let client_message = client_message.unwrap();
+        assert_eq!(client_message.data_type, data_type);
+        assert_eq!(client_message.channel, "channel_1");
+        assert_eq!(client_message.key, "key_1");
+        assert_eq!(client_message.value, value);
+        assert_eq!(client_message.tag, None);
+        assert_eq!(client_message.category, None);
+        assert_eq!(client_message.hub_id, None);
+        assert_eq!(client_message.publisher_id, None);
+    }
+
+    #[test]
+    fn aliases_work_during_deserialization() {
+        let serialized_message =
+            "{\"d\":10,\"chn\":\"channel_1\",\"key\":\"key_1\",\"val\":\"some text 2\"}";
+        let client_message = serde_json::from_str::<ClientMessage>(serialized_message);
+        let data_type = ClientMessageDataType::Data as u8;
+        let value = ClientMessageValue::Data(MessageValue::Text(String::from("some text 2")));
+        assert!(client_message.is_ok());
+        let client_message = client_message.unwrap();
+        assert_eq!(client_message.data_type, data_type);
+        assert_eq!(client_message.channel, "channel_1");
+        assert_eq!(client_message.key, "key_1");
+        assert_eq!(client_message.value, value);
+        assert_eq!(client_message.tag, None);
+        assert_eq!(client_message.category, None);
+        assert_eq!(client_message.hub_id, None);
+        assert_eq!(client_message.publisher_id, None);
     }
 }
